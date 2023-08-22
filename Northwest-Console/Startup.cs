466a89d447b94b86a28ae66d7c827b11a54ca470
Cameron Peterson <cam.peterson@protonmail.com>
2023-08-22 10:52:41 -0500

    Init

---
+++ Northwest-Console/Startup.cs
@@ -0,0 +1,22 @@
+namespace Northwest_Console
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+        }
+    }
+}
